72b7512e2dd6934c55b8d795dd7ed1de63f6e49c
mykyta.koriak <mykyta.koriak@onix-systems.com>
2024-11-01 22:10:41 +0200

    Some changes in start file

--- SignalBot.py
+++ SignalBot.py
@@ -1,5 +1,4 @@
 import subprocess
-import time
 import logging
 
 # Настраиваем логирование в файл
@@ -10,39 +9,18 @@ logging.basicConfig(
 )
 
 def run_server():
-    server = None
-    process_tasks = None
     try:
         # Запускаем сервер Django
         logging.info("Запуск сервера Django...")
-        server = subprocess.Popen(["python", "manage.py", "runserver"])
-
-        # Ждём немного, чтобы убедиться, что сервер запущен
-        time.sleep(2)
+        server = subprocess.Popen(["python", "manage.py", "runserver"], stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
 
         # Запускаем процесс фоновых задач
         logging.info("Запуск фоновых задач process_tasks...")
-        process_tasks = subprocess.Popen(["python", "manage.py", "process_tasks"])
+        process_tasks = subprocess.Popen(["python", "manage.py", "process_tasks"], stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
 
-        # Ожидаем завершения обоих процессов
-        server.wait()
-        process_tasks.wait()
-    except KeyboardInterrupt:
-        logging.info("Остановка серверов...")
-        if server:
-            server.terminate()
-        if process_tasks:
-            process_tasks.terminate()
+        logging.info("Сервер Django и процесс process_tasks успешно запущены на фоне.")
     except Exception as e:
-        logging.error(f"Произошла ошибка: {e}")
-    finally:
-        # Завершаем процессы, если они всё ещё работают
-        if server and server.poll() is None:
-            server.terminate()
-            logging.info("Сервер Django остановлен.")
-        if process_tasks and process_tasks.poll() is None:
-            process_tasks.terminate()
-            logging.info("Процесс фоновых задач process_tasks остановлен.")
+        logging.error(f"Произошла ошибка при запуске серверов: {e}")
 
 if __name__ == "__main__":
     run_server()
