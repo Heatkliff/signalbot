7813f2348a8362f2730533a23ec6e6ecb65bcf0a
mykyta.koriak <mykyta.koriak@onix-systems.com>
2024-11-04 12:07:30 +0200

    Fixed sender

--- crypto_signals/sender.py
+++ crypto_signals/sender.py
@@ -16,11 +16,6 @@ class SignalBot:
             local_time = message[2].astimezone(timezone(timedelta(hours=2)))
             text = f"Трейдер {message[0]} в {local_time.strftime('%H:%M:%S')} Сигнал:\n{message[1]}"
 
-            if os.getenv("RUNNING_ON_SERVER"):
-                # Проверка, отправлялось ли уже сообщение
-                if not await SentMessage.objects.filter(message_text=message[1]).aexists():
-                    await self.bot.send_message(chat_id=self.chat_id, text=text, parse_mode='HTML')
-                    await SentMessage.objects.acreate(message_text=message[1], trader_name={message[0]})
-            else:
-                print("Like sent")
+            if not await SentMessage.objects.filter(message_text=message[1]).aexists():
+                await self.bot.send_message(chat_id=self.chat_id, text=text, parse_mode='HTML')
                 await SentMessage.objects.acreate(message_text=message[1], trader_name={message[0]})
