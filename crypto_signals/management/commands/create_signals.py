803a273895d061f73c97f407403d3077c71740e9
mykyta.koriak <mykyta.koriak@onix-systems.com>
2024-11-09 02:39:36 +0200

    Added comparison 15m with 1h data for signals

--- crypto_signals/management/commands/create_signals.py
+++ crypto_signals/management/commands/create_signals.py
@@ -4,9 +4,8 @@ import time
 from django.core.management.base import BaseCommand
 import logging
 from crypto_signals.tools.analytic_upgraded import BingXChart
-from crypto_signals.tools.collector import TelegramSignalScraper
 from crypto_signals.tools.sender import SignalBot
-from crypto_signals.models import DataCollectionLog, TelegramConfig
+from crypto_signals.models import TelegramConfig
 
 
 class Command(BaseCommand):
@@ -23,34 +22,46 @@ class Command(BaseCommand):
             chart = BingXChart()
             symbols = chart.fetch_symbols()
 
-            # Задаем интервал для графиков
             chart.set_interval(interval='15m')
-
-            # Получаем данные аналитики
 
             for symbol in symbols:
                 dict_analysis = chart.generate_analytics(symbol=symbol, hours_ago=24)
                 if dict_analysis['trade_signal'] != None:
                     signals.append(dict_analysis)
 
+            chart.set_interval(interval='1h')
+
             messages = []
             for signal in signals:
                 message = "✅✅✅✅✅СИГНАЛ ОТ СИСТЕМЫ✅✅✅✅✅\n"
                 message += (f"Пара: {signal['trade_signal']['монета']} \n"
                             f"Направление: {signal['trade_signal']['направление']} \n"
-                            f"Вероятность: {signal['trade_signal']['вероятность отработки']} \n"
-                            f"точка входа: {float(signal['trade_signal']['точка входа'])} \n"
+                            f"Вероятность: {signal['trade_signal']['вероятность отработки']} \n")
+
+                dict_analysis = chart.generate_analytics(symbol=signal['trade_signal']['монета'], hours_ago=48)
+
+                if dict_analysis['trade_signal'] is not None:
+                    if dict_analysis['trade_signal']['направление'] == signal['trade_signal']['направление']:
+                        message += "✅✅✅СИГНАЛ СОВПАДАЕТ С ЧАСОВЫМ✅✅✅ \n"
+                    else:
+                        message += "❌❌❌СИГНАЛ НЕ СОВПАДАЕТ С ЧАСОВЫМ❌❌❌ \n"
+                else:
+                    message += "⚠️⚠️⚠️СИГНАЛ НЕ СУЩЕСТВУЕТ НА ЧАСОВОМ⚠️⚠️⚠️ \n"
+
+                message += (f"точка входа: {float(signal['trade_signal']['точка входа'])} \n"
                             f"тейк поинт: {float(signal['trade_signal']['тейк поинт'])} \n"
                             f"стоп-лосс: {float(signal['trade_signal']['стоп-лосс'])} \n")
                 message += "✅✅✅✅✅СИГНАЛ ОТ СИСТЕМЫ✅✅✅✅✅"
                 messages.append(message)
 
+            config = await TelegramConfig.objects.afirst()
+            bot = SignalBot(config=config)
             if len(messages) > 0:
-                config = await TelegramConfig.objects.afirst()
-                bot = SignalBot(config=config)
                 for message in messages:
                     await bot.send_message(message)
                     time.sleep(2)
+            else:
+                await bot.send_message("Новых сигналов в данный момент нет")
 
         loop = asyncio.new_event_loop()
         loop.run_until_complete(async_collect_and_send())
