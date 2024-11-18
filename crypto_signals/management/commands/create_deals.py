4c149e43b58139b04d3152d28c41811759c3f2ff
mykyta.koriak <mykyta.koriak@onix-systems.com>
2024-11-18 19:31:16 +0200

    Added autotrading

---
+++ crypto_signals/management/commands/create_deals.py
@@ -0,0 +1,100 @@
+from django.core.management.base import BaseCommand
+import logging
+from crypto_signals.tools.analytic_upgraded import BingXChart
+from crypto_signals.tools.autotrade import BingXTradingBot
+from crypto_signals.models import TelegramConfig
+
+
+class Command(BaseCommand):
+    help = 'Поиск новых сигналов и отправка их в группу'
+
+    def handle(self, *args, **kwargs):
+        self.stdout.write(self.style.SUCCESS("Запуск задачи collect_and_create_deals..."))
+        logger = logging.getLogger(__name__)
+        logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+        self.config = TelegramConfig.objects.first()
+
+        signals = []
+
+        chart = BingXChart()
+        symbols = chart.fetch_symbols()
+
+        chart.set_interval(interval='1h')
+
+        for symbol in symbols:
+            try:
+                dict_analysis = chart.generate_analytics(symbol=symbol, hours_ago=48)
+            except BaseException as e:
+                print(e)
+                continue
+            if dict_analysis['trade_signal'] != None:
+                signals.append(dict_analysis)
+
+        chart.set_interval(interval='15m')
+
+        messages = []
+        for signal in signals:
+            message = "✅✅✅✅✅СДЕЛКА СИСТЕМЫ✅✅✅✅✅\n"
+            message += (f"Пара: {signal['trade_signal']['монета']} \n"
+                        f"Направление: {signal['trade_signal']['направление']} \n"
+                        f"Вероятность: {signal['trade_signal']['вероятность отработки']} \n")
+
+            dict_analysis = chart.generate_analytics(symbol=signal['trade_signal']['монета'], hours_ago=48)
+
+            if dict_analysis['trade_signal'] is not None:
+                if dict_analysis['trade_signal']['направление'] == signal['trade_signal']['направление']:
+                    message += "✅✅✅СИГНАЛ СОВПАДАЕТ С 15m tf✅✅✅ \n"
+                    self.create_deal(
+                        symbol=signal['trade_signal']['монета'],
+                        position=signal['trade_signal']['направление'],
+                        entry_price=float(signal['trade_signal']['точка входа']),
+                        take_profit=float(signal['trade_signal']['тейк поинт']),
+                        stop_loss=float(signal['trade_signal']['стоп-лосс'])
+                    )
+                else:
+                    continue
+            else:
+                self.create_deal(
+                    symbol=signal['trade_signal']['монета'],
+                    position=signal['trade_signal']['направление'],
+                    entry_price=float(signal['trade_signal']['точка входа']),
+                    take_profit=float(signal['trade_signal']['тейк поинт']),
+                    stop_loss=float(signal['trade_signal']['стоп-лосс'])
+                )
+                message += "⚠️⚠️⚠️СИГНАЛ НЕ СУЩЕСТВУЕТ НА 15m tf⚠️⚠️⚠️ \n"
+
+            message += (f"точка входа: {float(signal['trade_signal']['точка входа'])} \n"
+                        f"тейк поинт: {float(signal['trade_signal']['тейк поинт'])} \n"
+                        f"стоп-лосс: {float(signal['trade_signal']['стоп-лосс'])} \n")
+            message += f"Коментарий от системы: \n {signal['trade_signal']['comment']} \n"
+            message += "✅✅✅✅✅СДЕЛКА СИСТЕМЫ✅✅✅✅✅"
+            messages.append(message)
+
+        if len(messages) > 0:
+            for message in messages:
+                print(message)
+        else:
+            print("Новых сделок в данный момент нет")
+        logger.info("Задача collect_and_create_deals выполнена")
+
+    def create_deal(self, symbol, position, entry_price, take_profit, stop_loss):
+        api_key = self.config.api_key
+        secret_key = self.config.secret_key
+
+        bot = BingXTradingBot(api_key, secret_key)
+
+        try:
+            response = bot.place_order(
+                symbol=symbol,
+                side="BUY",
+                position_side=position,
+                order_type="LIMIT",
+                quantity=1,
+                entry_price=entry_price,  # Оставляем None для рыночного ордера
+                take_profit=take_profit,
+                stop_loss=stop_loss,
+                leverage=25,
+            )
+            print("Order placed successfully:", response)
+        except Exception as e:
+            print("Error placing order:", e)
---
+++ crypto_signals/tools/autotrade.py
@@ -0,0 +1,89 @@
+import time
+import requests
+import hmac
+from hashlib import sha256
+
+
+class BingXTradingBot:
+    def __init__(self, api_key, secret_key, api_url="https://open-api.bingx.com"):
+        self.api_key = api_key
+        self.secret_key = secret_key
+        self.api_url = api_url
+
+    def _get_sign(self, payload):
+        """Генерация подписи HMAC SHA256"""
+        return hmac.new(
+            self.secret_key.encode("utf-8"), payload.encode("utf-8"), sha256
+        ).hexdigest()
+
+    def _send_request(self, method, path, params, payload=None):
+        """Отправка HTTP-запроса к API"""
+        params_str = "&".join([f"{k}={v}" for k, v in sorted(params.items())])
+        signature = self._get_sign(params_str)
+        url = f"{self.api_url}{path}?{params_str}&signature={signature}"
+        headers = {"X-BX-APIKEY": self.api_key}
+
+        response = requests.request(method, url, headers=headers, data=payload)
+        if response.status_code != 200:
+            raise Exception(f"API Error: {response.status_code}, {response.text}")
+        return response.json()
+
+    def place_order(
+            self,
+            symbol,
+            side,
+            position_side,
+            order_type,
+            quantity,
+            entry_price=None,
+            take_profit=None,
+            stop_loss=None,
+            leverage=1,
+    ):
+        """
+        Создание сделки
+        :param symbol: Торговая пара, например "BTC-USDT"
+        :param side: BUY или SELL
+        :param position_side: LONG или SHORT
+        :param order_type: MARKET или LIMIT
+        :param quantity: Сумма в USDT
+        :param entry_price: Цена входа (для лимитного ордера)
+        :param take_profit: Цена тейк-профита
+        :param stop_loss: Цена стоп-лосса
+        :param leverage: Множитель
+        :return: Ответ API
+        """
+        path = "/openApi/swap/v2/trade/order"
+
+        params = {
+            "symbol": symbol,
+            "side": side,
+            "positionSide": position_side,
+            "type": order_type,
+            "quantity": quantity,
+            "leverage": leverage,
+            "timestamp": int(time.time() * 1000),
+        }
+
+        if position_side == "LONG":
+            params['side'] = "BUY"
+        elif position_side == "SHORT":
+            params['side'] = "SELL"
+
+        # Для лимитного ордера добавляем entry_price
+        if order_type == "LIMIT" and entry_price:
+            params["price"] = entry_price
+            quantity_real = (quantity * leverage) / entry_price
+            params['quantity'] = quantity_real
+
+        # Добавляем тейк-профит и стоп-лосс в формате JSON строки
+        if take_profit:
+            params["takeProfit"] = (
+                f'{{"type": "TAKE_PROFIT_MARKET", "stopPrice": {take_profit}, "price": {take_profit}, "workingType": "MARK_PRICE"}}'
+            )
+        if stop_loss:
+            params["stopLoss"] = (
+                f'{{"type": "STOP_MARKET", "stopPrice": {stop_loss}, "price": {stop_loss}, "workingType": "MARK_PRICE"}}'
+            )
+
+        return self._send_request("POST", path, params)
